42fce929163a69a174df07368a94852f9f6bc52d
ambarpal <palambar.96@gmail.com>
2014-09-06 22:06:27 +0530

    dar de disko

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+	validates :title, :desc, :iurl, :price, presence: { message: 'dekha hai, want my shoe??' }
+	validates :price, numericality: {greater_than_or_equal_to: 1, message: 'yeh price hai ya mamu??'}
+	validates :iurl, allow_blank: true, format: {
+		with: %r{\.(gif|jpg|png)\Z}i,
+		message: 'kya likh raha hai be!?!'
+	}
+	#validates :
 end
